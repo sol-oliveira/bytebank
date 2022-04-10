931d75e6dc12bfa03f5e6d0f32a703b70eb4a87f
Sol <sol.oliveira@elevenfinancial.com>
2022-04-09 23:46:56 -0300

    add: formulário de nova transferência

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -8,10 +9,11 @@ import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferen
   declarations: [
     AppComponent,
     NovaTransferenciaComponent,
-    
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
