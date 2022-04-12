5ad8186bfe363c2a69539b3fae76006502e19ebd
Sol <sol.oliveira@elevenfinancial.com>
2022-04-12 08:50:40 -0300

    add: aula2/propagação de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ByteBank';
+  transferencia: any;
+
+  transferir($event) {
+    console.log($event);
+    this.transferencia = $event;
+  }
 }
